a4ac24dac2045ab4ddbfa9d028e559468c355ea4
Fabien V <fabien.vermeiren@gmail.com>
2022-03-31 10:30:19 +0200

    separate components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
-import './new.css';
+import React, { useState } from "react";
+import "./new.css";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 
 function App() {
   return (
-    <>
     <div className="App">
       <header className="App-header">
         <h1>My Todo App</h1>
       </header>
-      <input type="text" placeholder="Type a new todo"/>
-      <button>Add Todo</button>
-      <ToDoList/>
-    </div>
-    </>
-  );
-}
-
-const ToDoList = () => {
-  return (
-    <div>
-      <h2>Todos</h2>
-      <ul>
-        <li>
-          <label>
-          <input type="checkbox" />
-          {' '} todo 1
-        </label>
-        </li>
-        <li>
-          <label>
-          <input type="checkbox" />
-          {' '} todo 2
-        </label>
-        </li>
-      </ul>
+      <TodoForm />
+      <TodoList />
+      <div>
+        <div>0 left todo</div>
+        <div>
+          <a>All</a>
+        </div>
+        <div>
+          <a>Active</a>
+        </div>
+        <div>
+          <a>Completed</a>
+        </div>
+      </div>
     </div>
   );
 }
-
 
 export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function TodoList() {
+  return (
+    <div>
+        <ul>
+            <li>
+                <label><input type="checkbox"/> todo1 </label>
+            </li>
+            <li>
+                <label><input type="checkbox"/> todo1 </label>
+            </li>
+        </ul>
+    </div>
+  )
+}
