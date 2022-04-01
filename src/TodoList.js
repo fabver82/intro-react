f0a6181ecdb87c2542ca58d122582a91d504fa22
Fabien V <fabien.vermeiren@gmail.com>
2022-04-01 13:36:47 +0200

    refactor filterTodos

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,20 +2,20 @@ import React from "react";
 import Todo from "./Todo";
 
 export default function TodoList({ todos, toggleTodo, filter }) {
-  let filteredTodos = [];
-  if (filter === "active") {
-    filteredTodos = todos.filter((todo) => !todo.complete);
+  function filterTodos(filter) {
+    if (filter === "active") {
+      return todos.filter((todo) => !todo.complete);
+    }
+    if (filter === "complete") {
+      return todos.filter((todo) => todo.complete);
+    }
+    return todos;
   }
-  if (filter === "complete") {
-    filteredTodos = todos.filter((todo) => todo.complete);
-  }
-  if (filter === "all") {
-    filteredTodos = [...todos];
-  }
+
   return (
     <div>
       <ul>
-        {filteredTodos.map((todo) => {
+        {filterTodos(filter).map((todo) => {
           return <Todo key={todo.id} toggleTodo={toggleTodo} todo={todo} />;
         })}
       </ul>
