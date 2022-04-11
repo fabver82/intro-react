9ea1e3bba4bf8ce8576849c80bd6b85e09a0b9cd
Fabien V <fabien.vermeiren@gmail.com>
2022-04-11 15:40:53 +0200

    keep old code as comments before reordering, as example of using framer motion

--- src/Todo.js
+++ src/Todo.js
@@ -16,5 +16,15 @@ export default function Todo({ todo, toggleTodo }) {
         <span className="checkmark"></span> {todo.name}{" "}
       </label>
     </Reorder.Item>
+    // <li>
+    //   <label className="checkbox">
+    //     <input
+    //       type="checkbox"
+    //       onChange={handleTodoClick}
+    //       checked={todo.complete}
+    //     />
+    //     <span className="checkmark"></span> {todo.name}{" "}
+    //   </label>
+    // </li>
   );
 }
